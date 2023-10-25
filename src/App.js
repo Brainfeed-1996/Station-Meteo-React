15f4a845fa1bfb026fbecb88c78663b25a545dc8
Brainfeed-1996 <121355278+Brainfeed-1996@users.noreply.github.com>
2023-10-25 17:17:33 +0200

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import StationMeteo from './Components/StationMeteo/StationMeteo';
+
+
+function App() {
+  return (
+    <div className="App">
+      <StationMeteo />
+    </div>
+  );
+}
+
+export default App;
